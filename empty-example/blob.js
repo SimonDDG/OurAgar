8a64582eb4b7b14dd635a56473dc1c82d740fc0a
SimonDDG <simon.douchka@gmail.com>
2019-12-16 11:12:40 +0100

    created blobs, movment

---
+++ empty-example/blob.js
@@ -0,0 +1,40 @@
+/*
+
+function Blob (){
+  this.pos = createVector(640/2, 480/2);
+
+  this.show = function (){
+      fill(255);
+
+      ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+  }
+}
+
+
+/*
+
+class Blob {
+
+    constructor(width, height, r,){
+
+    }
+
+ 
+
+    this.pos = createVector(width/2, height/2);
+    this.r = 64;
+
+    
+
+    function Blob (){
+
+    this.show = function (){
+        fill(255);
+
+        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+    }
+}
+
+}
+
+*/
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,12 +1,45 @@
+
+var blob;
+
+var blobs = [];
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(600, 600);
+
+  blob = new Blob(width/2, height/2, 64);
+  for (let i = 0; i < 10; i++) {
+    blobs[i] = new Blob(random(width), random(height), 16);
+  }
+
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0);
+  blob.show();
+  blob.update();
+  for (let i = 0; i < blobs.length; i++) {
+    blobs[i].show();
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+ 
 }
+
+
+//Ha det separat vid behov.
+function Blob (x, y, r){
+  this.pos = createVector(x, y);
+  this.r = r;
+  this.update = function() {
+    var velocity = createVector(mouseX, mouseY);
+    velocity.sub(this.pos);
+    velocity.setMag(3);
+    this.pos.add(velocity);
+  }
+
+  this.show = function (){
+      fill(255);
+
+      ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+  }
+}
+
