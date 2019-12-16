414df3cc110b3b323fd8eeebef0a8137decc23e6
SimonDDG <simon.douchka@gmail.com>
2019-12-16 12:03:33 +0100

    created eat function, center position, larger playing field

--- empty-example/blob.js
+++ empty-example/blob.js
@@ -1,14 +1,23 @@
-function Blob (x, y, r){
+function Blob (x, y, r) {
     this.pos = createVector(x, y);
     this.r = r;
     this.update = function() {
-      var velocity = createVector(mouseX, mouseY);
-      velocity.sub(this.pos);
+      var velocity = createVector(mouseX-width/2, mouseY-height/2);
       velocity.setMag(3);
       this.pos.add(velocity);
     }
   
-    this.show = function (){
+this.eats = function (smallerBlob) {
+    let distance = p5.Vector.dist(this.pos, smallerBlob.pos);
+    if(distance < this.r + smallerBlob.r) {
+        this.r += smallerBlob.r*0.1;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+    this.show = function () {
         fill(255);
   
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -6,21 +6,25 @@ var blobs = [];
 function setup() {
   createCanvas(600, 600);
 
-  blob = new Blob(width/2, height/2, 64);
-  for (let i = 0; i < 10; i++) {
-    blobs[i] = new Blob(random(width), random(height), 16);
+  blob = new Blob(width / 2, height / 2, 64);
+  for (let i = 0; i < 60; i++) {
+    let x = random(-width, width * 2)
+    let y = random(-height, height * 2)
+    blobs[i] = new Blob(x, y, 16);
   }
 
 }
 
 function draw() {
   background(0);
-  translate(width/2 - blob.pos.x, height/2 - blob.pos.y);
-  blob.show();
-  blob.update();
-  for (let i = 0; i < blobs.length; i++) {
+  translate(width / 2 - blob.pos.x, height / 2 - blob.pos.y);
+  for (let i = blobs.length-1; i >= 0; i--) {
+    blob.show();
+    if(blob.eats(blobs[i])){
+      blobs.splice(i,1);
+    }
     blobs[i].show();
   }
+  blob.update();
 
- 
 }
